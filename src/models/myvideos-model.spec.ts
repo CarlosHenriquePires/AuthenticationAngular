03fe6605483471789daba3a3c82299cc1ced4149
carluxhenrique <carluxhenrique@gmail.com>
2020-06-15 17:22:15 -0300

    Revert "Criando um componente para listar videos"
    This reverts commit 6cfa79fc29a227b77b193fb147b95a17e0c34554.

--- src/models/myvideos-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MyvideosModel } from './myvideos-model';
-
-describe('MyvideosModel', () => {
-  it('should create an instance', () => {
-    expect(new MyvideosModel()).toBeTruthy();
-  });
-});
